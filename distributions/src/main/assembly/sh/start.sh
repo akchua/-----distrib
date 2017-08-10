959e34709fe4420b364edee71e744270b702cabc
Jasper Chua <adrianjasperchua@gmail.com>
2017-08-10 09:46:18 +0800

    version 1.9.12.QUICKFIX
    [GIT]
    	Fixing repo

--- distributions/src/main/assembly/sh/start.sh
+++ distributions/src/main/assembly/sh/start.sh
@@ -1,1 +1,1 @@
-java -Dloader.path=/usr/local/primepad/config -jar -Xmx3072m ../bin/distributions-1.9.8.QUICKFIX.jar
+java -Dloader.path=/usr/local/primepad/config -jar -Xmx3072m ../bin/distributions-1.9.12.QUICKFIX.jar
