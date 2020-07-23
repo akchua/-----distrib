6219b434da97f4da27c4e201cdba5dd4f6cf5113
Jasper Chua <adrianjasperchua@gmail.com>
2020-07-23 14:38:22 +0800

    version 2.1.3
    [Update]
    	Added 10 and 1000 transfer @purchaseOrderTransfer

--- distributions/src/main/assembly/sh/start.sh
+++ distributions/src/main/assembly/sh/start.sh
@@ -1,1 +1,1 @@
-java -Dloader.path=/usr/local/primepad/config -jar -Xmx3072m ../bin/distributions-2.1.0.jar
+java -Dloader.path=/usr/local/primepad/config -jar -Xmx3072m ../bin/distributions-2.1.2.jar
